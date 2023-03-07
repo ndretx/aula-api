c1f3e939627189358eed16f76e7fa08080566e32
André Chaves Teixeira <TRI0106062@aluno.firjansenaisesi.com.br>
2023-03-07 19:21:38 -0300

    primeiro commit

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // inicializadora do back-end
   const app = await NestFactory.create(AppModule);
+  // criando uma instancia do nest, passando um parametro da cofiguraçao do appModule
   await app.listen(3000);
 }
 bootstrap();
